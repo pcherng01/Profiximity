4c8935f4c45a3411a3326464e99744d5a7490fb8
pcherng01 <LeafCherngchaosil@Pongsakorns-MacBook-Pro.local>
2015-04-04 03:06:21 -0700

    Initial Commit

---
+++ Profiximity/Profiximity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Profiximity
+//
+//  Created by Pongsakorn Cherngchaosil on 4/4/15.
+//  Copyright (c) 2015 Pongsakorn Cherngchaosil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Profiximity/Profiximity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Profiximity
+//
+//  Created by Pongsakorn Cherngchaosil on 4/4/15.
+//  Copyright (c) 2015 Pongsakorn Cherngchaosil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
